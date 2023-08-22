6b5592e7a83e8446ae55dc89f5c692e49ebb94b0
Marta Domínguez Moraga <martadom@MacBook-Air-de-Marta.local>
2023-08-22 17:43:03 +0200

    version 1.0.7

--- index.js
+++ index.js
@@ -6,39 +6,60 @@ const {
     accessControl
 } = require('./lib/_helpers');
 
-const {    
-    initializeFirebase,
-    addDoc,
-    app,
-    auth,
-    collection,
-    collectionGroup,
-    db,
-    doc,
-    firestore,
-    functions,
-    get,
-    getDoc,
-    getDocs,
-    onAuthStateChanged,
-    orderBy,
-    query,
-    ref,
-    set,
-    signInWithEmailAndPassword,
-    where
-} = require('./firebase');
 
-function initialize(firebaseConfig) {
-    initializeFirebase(firebaseConfig);
+let    addDoc;
+let    app;
+let    auth;
+let    collection;
+let    collectionGroup;
+let    db;
+let    doc;
+let    firestore;
+let    functions;
+let    get;
+let    getDoc;
+let    getDocs;
+let    onAuthStateChanged;
+let    orderBy;
+let    query;
+let    ref;
+let    set;
+let    signInWithEmailAndPassword;
+let     where;
+
+
+function importFbFunctions(
+    addDoc=addDoc,
+    app=app,
+    auth=auth,
+    collection=collection,
+    collectionGroup=collectionGroup,
+    db=db,
+    doc=doc,
+    firestore=firestore,
+    functions=functions,
+    get=get,
+    getDoc=getDoc,
+    getDocs=getDocs,
+    onAuthStateChanged=onAuthStateChanged,
+    orderBy=orderBy,
+    query=query,
+    ref=ref,
+    set=set,
+    signInWithEmailAndPassword=signInWithEmailAndPassword,
+    where=where) {
+
 }
+
+
+
 
 module.exports = {
     getSessions,
     calculateProbabilities,
     improveUserExperience,
     accessControl,
-    initialize,
+    importFbFunctions,
     addDoc,
     app,
     auth,
