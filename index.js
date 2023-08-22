9fbc001b79aa4b5181b96f0beeb92b2691a1a29c
Marta Domínguez Moraga <martadom@MacBook-Air-de-Marta.local>
2023-08-22 16:16:26 +0200

    version 1.0.1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
-const { helperFunction1, helperFunction2 } = require('./lib/_helpers');
+const {     getSessions,
+    calculateProbabilities,
+    improveUserExperience,
+    accessControl } = require('./lib/_helpers');
 
 module.exports = {
     getSessions,
